8a00ad37c4f317a89bb870c00613b14fcf3d6842
thedevopsjedi <thedevopsjedi@gmail.com>
2023-07-22 11:55:32 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# thedevopsjedi.github.io Running bilberry-hugo-theme v3.24.1
---
+++ init-cookieconsent.js
@@ -0,0 +1,19 @@
+window.cookieconsent.initialise({
+    "palette": {
+        "popup": {
+            "background": "#EE6C4D",
+            "text": "#FFFFFF"
+        },
+        "button": {
+            "background": "#F4F4F4",
+            "text": "#0E4D8B"
+        }
+    },
+    "theme": "classic",
+    "content": {
+        "message": "This website uses cookies to enable Google Analytics. No personally identifiable information (PII) is collected. By continuing to browse this site you consent to this.",
+        "dismiss": "Got it!",
+        "link": "Learn more",
+        "href": "/privacy"
+    }
+})
